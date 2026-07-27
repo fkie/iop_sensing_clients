d5cae78420edb390fd5b29bfacd3be045523d939
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2026-07-27 09:25:41 +0200

    fix for rolling build

--- fkie_iop_client_costmap2d/src/urn_jaus_jss_iop_CostMap2DClient/CostMap2DClient_ReceiveFSM.cpp
+++ fkie_iop_client_costmap2d/src/urn_jaus_jss_iop_CostMap2DClient/CostMap2DClient_ReceiveFSM.cpp
@@ -44,9 +44,7 @@ CostMap2DClient_ReceiveFSM::CostMap2DClient_ReceiveFSM(std::shared_ptr<iop::Comp
      */
     context = new CostMap2DClient_ReceiveFSMContext(*this);
 
-    this->p_tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(
-        cmp->get_node_parameters_interface(),
-        cmp->get_node_topics_interface());
+    this->p_tf_broadcaster = std::make_unique<tf2_ros::TransformBroadcaster>(cmp);
     this->pAccessControlClient_ReceiveFSM = pAccessControlClient_ReceiveFSM;
     this->pEventsClient_ReceiveFSM = pEventsClient_ReceiveFSM;
     this->pTransport_ReceiveFSM = pTransport_ReceiveFSM;
